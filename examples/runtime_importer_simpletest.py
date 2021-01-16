3a6950dc7509592872f341bed3d73a002478c1f0
Jonah Yolles-Murphy <TGTechie01@gmial.com>
2021-01-16 12:19:18 -0500

    correct some names

--- examples/runtime_importer_simpletest.py
+++ examples/runtime_importer_simpletest.py
@@ -1,15 +1,14 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 Jonah Y-M
 #
 # SPDX-License-Identifier: Unlicense
-
 
 import runtime_importer
 
 # ask the user what module to import
 mod = runtime_importer.importpath(input('path to import module (time time ):'))
 
+# use the module as usual
 print(mod)
 
-# use the module as usual
-
+# when youre done with it free it.
 mod.clear_module()
